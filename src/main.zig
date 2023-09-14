219fc5a6b889871510ef9dd6ccf1ac3551430ee5
Matheus C. França <matheus-catarino@hotmail.com>
2023-09-14 12:41:39 -0300

    some fixes

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,22 @@
 const std = @import("std");
 const binding = @import("binding");
-const print = std.log.info;
+
+const log = std.log.scoped(.ffi);
+
+// override the std implementation
+pub const std_options = struct {
+    pub const log_level = .info;
+};
 
 pub fn main() !void {
-    print("Initializing shared data...\n", .{});
+    log.info("Initializing shared data...\n", .{});
     binding.init_shared_data();
 
-    print("Starting Rust Tokio event loop...\n", .{});
-    binding.start_event_loop(10); // Set the counter limit to 10
+    defer {
+        log.info("Shutting down the Rust Tokio event loop...\n", .{});
+        binding.shutdown_event_loop();
+    }
 
-    print("Shutting down the Rust Tokio event loop...\n", .{});
-    binding.shutdown_event_loop();
+    log.info("Starting Rust Tokio event loop...\n", .{});
+    binding.start_event_loop(10); // Set the counter limit to 10
 }
